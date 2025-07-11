c3d829841822c82bbbb3afdcd850863d4b4fb69e
Jaser1010 <j.osama2101@nu.edu.eg>
2025-07-11 18:44:28 +0300

    Q1 - Print all numbers between 1 and a given number.

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Assignment_4
+﻿using System;
+using System.Text;
+
+namespace Assignment_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1 - Print all numbers between 1 and a given number.
+            /*
+            Console.WriteLine("Q1: Print numbers from 1 to N");
+            Console.Write("Input: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Output: ");
+            for (int i = 1; i <= number1; i++)
+            {
+                Console.Write(i);
+                if (i < number1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            */
+            #endregion
         }
     }
 }
